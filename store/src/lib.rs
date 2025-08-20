ce1c227ebc77ee4bf099e6597a61a45560c5df99
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-08-21 00:32:02 +0530

    wrote sql and migrated

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,4 +1,22 @@
-pub struct Store {}
+// this is like a class
+pub struct Store {
+    conn: Connection,
+}
+
+// we can write from scratch how to connect to a db what bytes to send but we would be using a library
+// sqlx or diesel
+
+// follow this up
+// https://diesel.rs/guides/getting-started
+
+impl Default for Store {
+    // this is like the constructor function which is called when the object is created
+    fn default() -> Self {
+        Self {
+            conn: Connection::open("sqlite:///db.sqlite").unwrap(),
+        }
+    }
+}
 
 impl Store {
     pub fn create_user(&self) {
